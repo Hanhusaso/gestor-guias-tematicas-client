a18fe8d68b8d45ea3af08b96da4ea96ad9ff9b06
HaroldHuaccha <haroldhuaccha2498@gmail.com>
2021-04-19 11:52:32 -0500

    return to initial

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Image } from 'semantic-ui-react'
+import Guia from "../components/Dashboard/Content/guia";
 
 function dashboard() {
 
@@ -8,31 +9,43 @@ function dashboard() {
     
     // const { activeItem } = this.state
     
-    const [activeItem, setActiveItem] = useState('home');
+    const [activeItem, setActiveItem] = useState('MIS GUIAS TEMÁTICAS');
 
     const handleItemClick = (e, { name }) => setActiveItem(name)
 
     return (
-        <div className="dashboard">
-            <nav className="menu-nav">
-                <Menu pointing secondary vertical>
-                    <Menu.Item
-                    name='home'
-                    active={activeItem === 'home'}
-                    onClick={handleItemClick}
-                    />
-                    <Menu.Item
-                    name='messages'
-                    active={activeItem === 'messages'}
-                    onClick={handleItemClick}
-                    />
-                    <Menu.Item
-                    name='friends'
-                    active={activeItem === 'friends'}
-                    onClick={handleItemClick}
-                    />
-                </Menu>
-            </nav>
+        <div>
+            <div className="dashboard">
+                {/* <Image src="/logo-dashboard.svg" alt="logo-dashboard" /> */}
+                <nav className="menu-nav">
+                    
+                    <Menu pointing secondary vertical>
+                        <Menu.Item
+                        name='MIS GUIAS TEMÁTICAS'
+                        active={activeItem === 'MIS GUIAS TEMÁTICAS'}
+                        onClick={handleItemClick}
+                        />
+                        <Menu.Item
+                        name='INGRESAR NUEVA GUÍA TEMÁTICA'
+                        active={activeItem === 'INGRESAR NUEVA GUÍA TEMÁTICA'}
+                        onClick={handleItemClick}
+                        />
+                        {/* <Menu.Item
+                        name='friends'
+                        active={activeItem === 'friends'}
+                        onClick={handleItemClick}
+                        /> */}
+                    </Menu>
+                </nav>
+
+                
+            </div>
+
+                {activeItem === 'INGRESAR NUEVA GUÍA TEMÁTICA' ? (
+                    <Guia />
+                ): null}
+                
+                    
         </div>
     )
 }
