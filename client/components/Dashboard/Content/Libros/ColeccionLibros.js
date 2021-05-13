75433e740b7deffdadb927b6c30e9986b075b6f9
HaroldHuaccha <haroldhuaccha2498@gmail.com>
2021-05-13 07:13:54 -0500

    Intefaz y validaciones del formulario de editar los recursos de libros a un 85%

--- client/components/Dashboard/Content/Libros/ColeccionLibros.js
+++ client/components/Dashboard/Content/Libros/ColeccionLibros.js
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { Menu } from 'semantic-ui-react'
-import { List, Image, Icon } from "semantic-ui-react";
+import { List, Image, Icon, Modal, Button, Header } from "semantic-ui-react";
 import CreateColectionModal from '../../Modal/CreateColectionModal';
 import DeleteColectionModal from '../../Modal/DeleteColectionModal';
 
-export default function BaseDatos() {
+export default function ColeccionLibros(props) {
+
+    const {edit, setEdit} = props;
+    const openEdit = () => setEdit(true);
 
     const [showModal, setShowModal] = React.useState(false);
     const openShowModal = () => { setShowModal(true)}
 
     const [showModal2, setShowModal2] = React.useState(false);
     const openShowModal2 = () => { setShowModal2(true)}
-
+    
     return (
         <div className="colections padding-top-46">
             <div className="colections__header">
                 <h3 className="colections__header__title">COLECCIONES</h3>
-                <div className="colections__header__button" onClick={openShowModal}><Icon name="add circle"/> <h3 className="colections__header__create">Crear Colección</h3></div>
-                {/* <Button
-                    content=''
-                    disabled={open}
-                    // positive
-                    onClick={prueba}
-                /> */}
-                
+                <div className="colections__header__button" onClick={openShowModal}><Icon name="add circle"/> <h3 className="colections__header__create">Crear Colección</h3></div>     
             </div>
 
             <div className="colections__content container-30 padding-top-46">
                 <div className="colections__content__element padding-bottom-25">
                     <div className="colections__content__element__box container-46">
                         <h3 className="m-0">Colección impresa</h3>
-                        <div className="colections__content__element__box__button">
+                        <div className="colections__content__element__box__button" onClick={openEdit}>
                             Editar
                         </div>
                     </div>
@@ -40,7 +36,7 @@ export default function BaseDatos() {
                 <div className="colections__content__element padding-bottom-25">
                     <div className="colections__content__element__box container-46">
                         <h3 className="m-0">Fondo Editorial</h3>
-                        <div className="colections__content__element__box__button">
+                        <div className="colections__content__element__box__button" onClick={openEdit}>
                             Editar
                         </div>
                     </div>
@@ -51,7 +47,6 @@ export default function BaseDatos() {
             <CreateColectionModal show = {showModal} setShow={setShowModal} />
 
             <DeleteColectionModal show2 = {showModal2} setShow2={setShowModal2} />
-
         </div>
     )
 }
